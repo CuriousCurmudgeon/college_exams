227b2ae36bce8266a7234f1b8703744b69e720ee
Brian Meeker <meeker.brian@gmail.com>
2021-06-06 13:45:41 -0400

    First rough pass at the controller action to create an assigned exam.
    Known shortcomings include
    * No error messages included in 400 response.
    * The API request model should probably be abstracted. Just a lot of random symbols being used.
    * No strong parameters.

--- college_exams/app/controllers/assigned_exams_controller.rb
+++ college_exams/app/controllers/assigned_exams_controller.rb
@@ -1,4 +1,27 @@
 class AssignedExamsController < ApplicationController
   def create
+    # Load the exam by ID and college ID
+    exam = Exam.find_by id: params[:exam_id], college_id: params[:college_id]
+    # Check if we got a result, 400 if no result
+    if exam.nil? || !exam.in_exam_window?(DateTime.parse(params[:start_time]))
+      render :status => :bad_request and return
+    end
+
+    # Load the user that matches the name and phone number
+    user = User.find_by first_name: params[:first_name], last_name: params[:last_name], phone_number: params[:phone_number]
+    if user.nil?
+      # Create a new user if a match was not found
+      user = User.new
+      user.first_name = params[:first_name]
+      user.last_name = params[:last_name]
+      user.phone_number = params[:phone_number]
+    end
+    
+    # Assign the exam to the new user and save
+    exam.users << user
+    exam.save
+
+    # return 200
+    render :status => :ok
   end
 end
--- college_exams/test/controllers/assigned_exams_controller_test.rb
+++ college_exams/test/controllers/assigned_exams_controller_test.rb
@@ -1,8 +1,88 @@
 require "test_helper"
 
 class AssignedExamsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get assigned_exams_create_url
+  test "post should create assigned exam" do
+    user = User.first
+    exam = Exam.first
+    assert_difference("AssignedExam.count") do
+      post assigned_exams_path, params: { 
+        first_name: user.first_name,
+        last_name: user.last_name,
+        phone_number: user.phone_number, 
+        college_id: College.first.id,
+        exam_id: exam.id,
+        start_time: exam.start_time + 1.day}, as: :json
+    end
+    
     assert_response :success
+  end
+
+  test "post should not create new user if matching user found" do
+    user = User.first
+    exam = Exam.first
+    assert_no_difference("User.count") do
+      post assigned_exams_path, params: { 
+        first_name: user.first_name,
+        last_name: user.last_name,
+        phone_number: user.phone_number, 
+        college_id: College.first.id,
+        exam_id: exam.id,
+        start_time: exam.start_time + 1.day}, as: :json
+    end
+    
+    assert_response :success
+  end
+
+  test "post should create new user if matching user not found" do
+    user = User.first
+    exam = Exam.first
+    assert_difference("User.count") do
+      post assigned_exams_path, params: { 
+        first_name: "unknown",
+        last_name: user.last_name,
+        phone_number: user.phone_number, 
+        college_id: College.first.id,
+        exam_id: exam.id,
+        start_time: exam.start_time + 1.day}, as: :json
+    end
+    
+    assert_response :success
+  end
+
+  test "post should return 400 if exam_id invalid" do
+    post assigned_exams_path, params: { 
+      first_name: "Devin",
+      last_name: "Townsend",
+      phone_number: "0293840239", 
+      college_id: College.first.id,
+      exam_id: "123",
+      start_time: "2021-06-07"}, as: :json
+
+      assert_response :bad_request
+  end
+
+  test "post should return 400 if college_id invalid" do
+    post assigned_exams_path, params: { 
+      first_name: "Devin",
+      last_name: "Townsend",
+      phone_number: "0293840239", 
+      college_id: "123",
+      exam_id: Exam.first.id,
+      start_time: "2021-06-07"}, as: :json
+
+      assert_response :bad_request
+  end
+
+  test "post should return 400 if start_time not in exam window" do
+    exam = Exam.first
+    post assigned_exams_path, params: { 
+      first_name: "Devin",
+      last_name: "Townsend",
+      phone_number: "0293840239", 
+      college_id: College.first.id,
+      exam_id: exam.id,
+      start_time: exam.start_time - 1.day}, as: :json
+
+      assert_response :bad_request
   end
 end
