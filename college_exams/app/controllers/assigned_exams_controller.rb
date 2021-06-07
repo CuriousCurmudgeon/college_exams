bf24fee777d7a9ddb4863390daf94d36abdfd5bd
Brian Meeker <meeker.brian@gmail.com>
2021-06-06 20:17:17 -0400

    Use permitted params to simplify getting the user and creating a new one if necessary.

--- college_exams/app/controllers/assigned_exams_controller.rb
+++ college_exams/app/controllers/assigned_exams_controller.rb
@@ -1,27 +1,25 @@
 class AssignedExamsController < ApplicationController
   def create
-    # Load the exam by ID and college ID
     exam = Exam.find_by id: params[:exam_id], college_id: params[:college_id]
-    # Check if we got a result, 400 if no result
+    
     if exam.nil? || !exam.in_exam_window?(DateTime.parse(params[:start_time]))
       render :status => :bad_request and return
     end
 
-    # Load the user that matches the name and phone number
-    user = User.find_by first_name: params[:first_name], last_name: params[:last_name], phone_number: params[:phone_number]
+    user = User.find_by(user_params)
     if user.nil?
-      # Create a new user if a match was not found
-      user = User.new
-      user.first_name = params[:first_name]
-      user.last_name = params[:last_name]
-      user.phone_number = params[:phone_number]
+      user = User.new(user_params)
     end
     
-    # Assign the exam to the new user and save
     exam.users << user
     exam.save
 
-    # return 200
     render :status => :ok
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :phone_number)
   end
 end
