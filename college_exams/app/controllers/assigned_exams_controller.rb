396a40b8d1e1f0cd75d546defae4956c40017ee2
Brian Meeker <meeker.brian@gmail.com>
2021-06-06 20:45:37 -0400

    Broke out the exam validation into a before_action
    I'm indifferent about this change, but I'm playing around with ideas. Feels like six of one, half-dozen of another in such a simple use case.

--- college_exams/app/controllers/assigned_exams_controller.rb
+++ college_exams/app/controllers/assigned_exams_controller.rb
@@ -1,33 +1,37 @@
 class AssignedExamsController < ApplicationController
   include Response
 
+  before_action :validate_exam
+
   def create
-    exam = Exam.find_by id: params[:exam_id]
-    
-    if exam.nil?
-      render_bad_request('Exam not found') and return
-    end
-
-    if exam.college_id != params[:college_id]
-      render_bad_request('Exam is for different college') and return
-    end
-
-    if !exam.in_exam_window?(DateTime.parse(params[:start_time]))
-      render_bad_request('Start time not in exam window') and return
-    end
-
     user = User.find_by(user_params)
     if user.nil?
       user = User.new(user_params)
     end
     
-    exam.users << user
-    exam.save
+    @exam.users << user
+    @exam.save
 
     render :status => :ok
   end
 
   private
+
+  def validate_exam
+    @exam = Exam.find_by id: params[:exam_id]
+    
+    if @exam.nil?
+      render_bad_request('Exam not found') and return
+    end
+
+    if @exam.college_id != params[:college_id]
+      render_bad_request('Exam is for different college') and return
+    end
+
+    if !@exam.in_exam_window?(DateTime.parse(params[:start_time]))
+      render_bad_request('Start time not in exam window') and return
+    end
+  end
 
   def user_params
     params.permit(:first_name, :last_name, :phone_number)
